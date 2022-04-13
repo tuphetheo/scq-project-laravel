03e746c8ebd52c1fe34d48b31d4b9043754a2c8c
Théo Tuphé <tuphetheo@gmail.com>
2022-04-13 09:55:47 +0200

    Change Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReplyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +25,25 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+// group the following routes by auth middleware - you have to be signed-in to proceeed
+Route::group(['middleware' => 'auth'], function () {
+    // Dashboard
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Posts resourcefull controllers routes
+    Route::resource('posts', PostController::class);
+
+    // Comments routes
+    Route::group(['prefix' => '/comments', 'as' => 'comments.'], function () {
+        // store comment route
+        Route::post('/{post}', [CommentController::class, 'store'])->name('store');
+    });
+
+    // Replies routes
+    Route::group(['prefix' => '/replies', 'as' => 'replies.'], function () {
+        // store reply route
+        Route::post('/{comment}', [ReplyController::class, 'store'])->name('store');
+    });
+});
